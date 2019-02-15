e42512e33c37befa3023b4ed0d2bb7705b0c0c2f
joepound <14357797+joepound@users.noreply.github.com>
2019-02-14 17:44:29 -0700

    User PUT endpoint completed

--- middleware/routes/users/errors.js
+++ middleware/routes/users/errors.js
@@ -1,22 +1,26 @@
 module.exports = {
   GET_ALL_USERS_FAILURE: {
     title: "User GET (all): failure",
-    description: "Could not retrieve user database information."
+    description:
+      "Could not retrieve user database information due to an internal server error."
   },
 
   GET_INDIVIDUAL_USER_NOT_FOUND: {
     title: "User GET (individual): not found",
-    description: "No user with the supplied ID exists."
+    description: "No user with the supplied ID exists.",
+    recoveryInstructions: "Please ensure that the correct user ID is supplied."
   },
 
   GET_INDIVIDUAL_USER_FAILURE: {
     title: "User GET (individual): failure",
-    description: "Could not retrieve information on the specified user. "
+    description:
+      "Could not retrieve information on the specified user due to an internal server error."
   },
 
   GET_USER_POSTS_FAILURE: {
     title: "User posts GET: failure",
-    description: "Could not retrieve posts by the specified user."
+    description:
+      "Could not retrieve posts by the specified user due to an internal server error."
   },
 
   POST_USER_NO_NAME: {
@@ -31,14 +35,58 @@ module.exports = {
     recoveryInstructions: "Please supply a different name and try again."
   },
 
-  POST_USER_DUPLICATES_CHECK_FAILURE: {
-    title: "User POST: cannot check for duplicates",
+  POST_USER_DUPLICATE_NAMES_CHECK_FAILURE: {
+    title: "User POST: cannot check names in use",
     description:
-      "Could not create user - operation aborted because the user database could not be scanned for duplicate names."
+      "Could not create user - operation aborted because the user database could not be scanned for names in use."
   },
 
   POST_USER_FAILURE: {
     title: "User POST: failure",
-    description: "Could not create user."
+    description: "Could not create user due to an internal server error."
+  },
+
+  PUT_USER_NO_NAME: {
+    title: "User PUT: missing name",
+    description: "No name was supplied for updating the specified user's information.",
+    recoveryInstructions: "Please supply a name and try again."
+  },
+
+  PUT_USER_NOT_FOUND: {
+    title: "User PUT: user not found",
+    description: "No user with the supplied ID exists",
+    recoveryInstructions: "Please ensure that the correct user ID is supplied."
+  },
+
+  PUT_USER_EXISTENCE_CHECK_FAILURE: {
+    title: "User PUT: could not check user information",
+    description: "Could not check the user database due to an internal server error."
+  },
+
+  PUT_USER_NAME_IN_USE: {
+    title: "User PUT: name in use",
+    description: "The new name supplied for the specified user is already in use.",
+    recoveryInstructions: "Please supply a different name and try again."
+  },
+
+  PUT_USER_DUPLICATE_NAMES_CHECK_FAILURE: {
+    title: "User PUT: cannot check names in use",
+    description:
+      "Could not update the specified user - operation aborted because the user database could not be scanned for names in use."
+  },
+
+  PUT_USER_NO_UPDATED_ENTRIES: {
+    title: "User PUT: no user entries updated",
+    description: "The specified user was found in the user database but could not be updated due to an internal server error."
+  },
+
+  PUT_USER_MULTIPLE_UPDATED_ENTRIES: {
+    title: "User PUT: multiple user entries updated",
+    description: "Multiple user entries were updated due to an internal server error. We are sorry for the inconvenience."
+  },
+
+  PUT_USER_FAILURE: {
+    title: "User PUT: failure",
+    description: "Could not update user due to an internal server error."
   }
 };
--- middleware/routes/users/routes.js
+++ middleware/routes/users/routes.js
@@ -107,8 +107,9 @@ router.post("/", async (req, res) => {
 
   const { name } = req.body;
 
+  console.log("Checking if name was supplied...");
   if (name) {
-    console.log("Checking if user is unique...");
+    console.log("Checking if supplied name is unique...");
     try {
       const users = await userDB.get();
 
@@ -120,7 +121,6 @@ router.post("/", async (req, res) => {
           code,
           error: errors.POST_USER_NAME_IN_USE
         });
-        console.log("User POST attempt finished.");
       } else {
         try {
           const user = await userDB.insert({ name });
@@ -128,15 +128,13 @@ router.post("/", async (req, res) => {
             success: true,
             user
           });
-        } catch {
+        } catch (err) {
           const code = 500;
           res.status(code).json({
             success: false,
             code,
             errorInfo: errors.POST_USER_FAILURE
           });
-        } finally {
-          console.log("User POST attempt finished.");
         }
       }
     } catch (err) {
@@ -144,7 +142,7 @@ router.post("/", async (req, res) => {
       res.status(code).json({
         success: false,
         code,
-        error: errors.POST_USER_DUPLICATES_CHECK_FAILURE
+        error: errors.POST_USER_DUPLICATE_NAMES_CHECK_FAILURE
       });
     } finally {
       console.log("User POST attempt finished.");
@@ -160,8 +158,86 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   console.log("\nAttempting to PUT user information updates...");
+
+  const { id } = req.params;
+  const { name } = req.body;
+
+  console.log("Checking if name was supplied...");
+  if (name) {
+    console.log("Checking if specified user exists...");
+    try {
+      const user = await userDB.getById(id);
+
+      if (user) {
+        console.log("Checking if supplied name is unique...");
+        try {
+          const users = await userDB.get();
+
+          const nameInUse = users.some(user => user.name === name);
+          if (nameInUse) {
+            const code = 400;
+            res.status(code).json({
+              success: false,
+              code,
+              error: errors.PUT_USER_NAME_IN_USE
+            });
+          } else {
+            const updateCount = await userDB.update(id, { name });
+
+            if (updateCount === 1) {
+              res.status(200).json({
+                success: true,
+                user
+              });
+            } else {
+              const code = 500;
+              res.status(code).json({
+                success: false,
+                code,
+                error:
+                  updateCount > 1
+                    ? errors.PUT_USER_MULTIPLE_UPDATED_ENTRIES
+                    : errors.PUT_USER_NO_UPDATED_ENTRIES
+              });
+            }
+          }
+        } catch (err) {
+          const code = 500;
+          res.status(code).json({
+            success: false,
+            code,
+            error: errors.PUT_USER_DUPLICATE_NAMES_CHECK_FAILURE
+          });
+        }
+      } else {
+        const code = 404;
+        res.status(code).json({
+          success: false,
+          code,
+          error: errors.PUT_USER_NOT_FOUND
+        });
+      }
+    } catch {
+      const code = 500;
+      res.status(code).json({
+        success: false,
+        code,
+        error: errors.PUT_USER_EXISTENCE_CHECK_FAILURE
+      });
+    } finally {
+      console.log("User PUT attempt finished.");
+    }
+  } else {
+    const code = 400;
+    res.status(code).json({
+      success: false,
+      code,
+      errorInfo: errors.PUT_USER_NO_NAME
+    });
+    console.log("User PUT attempt finished.");
+  }
 });
 
 module.exports = router;
